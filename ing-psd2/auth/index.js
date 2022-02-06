1992cbecfc56a85819cd891e7573a2becb4a7643
Andrei Popa <andypopa@gmail.com>
2022-02-06 11:57:18 +0200

    feat: authorizationServerUrl works

--- ing-psd2/auth/index.js
+++ ing-psd2/auth/index.js
@@ -6,12 +6,12 @@ export const token = async () => {
 	return ingPsd2HttpClient.post(routes.auth.token, payload);
 }
 
-// to do:
-//   authorization headers for this request should have the token
-//   instead of the signature
+
 export const authorizationServerUrl = async () => {
-	const payload = 'scope=payment-accounts%3Abalances%3Aview%20payment-accounts%3Atransactions%3Aview&redirect_uri=https://www.example.com&country_code=NL';
-	return ingPsd2HttpClient.get(routes.auth.authorizationServerUrl, payload);
+	// to do:
+	//   use payload instead of string
+	const payload = '?scope=payment-accounts%3Abalances%3Aview%20payment-accounts%3Atransactions%3Aview&redirect_uri=https://www.example.com&country_code=NL';
+	return ingPsd2HttpClient.get(routes.auth.authorizationServerUrl + payload);
 }
 
 export const auth = {
